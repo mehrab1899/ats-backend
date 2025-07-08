13f6074b93f372e53e28b84531f1c8bb1e82b439
mehrab1899 <mehrabmehmood1899@gmail.com>
2025-07-08 23:57:18 +0100

    refactor:jobs module was refactored to have single Job typeDef

--- src/graphql/modules/job/typeDefs.ts
+++ src/graphql/modules/job/typeDefs.ts
@@ -15,43 +15,21 @@ export const jobTypeDefs = gql`
     CONTRACT
   }
 
-  type PublicJob {
-    id: ID!
-    title: String!
-    description: String!
-    status: JobStatus!
-    skillsRequired: JSON!
-    benefits: JSON!
-    createdAt: String!
-    context: String
-  }
-
-  type AdminJob {
-    id: ID!
-    title: String!
-    description: String!
-    status: JobStatus!
-    type: JobType!
-    applicants: Int!
-    createdAt: String!
-    context:String
-  }
-
   type Job {
     id: ID!
     title: String!
     description: String!
     status: JobStatus!
     type: JobType!
+    applicants: Int!
     skillsRequired: JSON!
     benefits: JSON!
     createdAt: String!
-    applicants: Int!
-    context:String
+    context: String
   }
 
   type JobsResponse {
-    jobs: [AdminJob!]!
+    jobs: [Job!]!
     totalJobsCount: Int!
   }
 
@@ -65,20 +43,19 @@ export const jobTypeDefs = gql`
   }
 
   type Query {
-    publicJobs: [PublicJob!]!
+    publicJobs: [Job!]!
     jobs(
       search: String
       status: JobStatus
       skip: Int = 0
       take: Int = 10
     ): JobsResponse!
-    getJobById(id: ID!): Job! 
-
+    getJobById(id: ID!): Job!
   }
 
   type Mutation {
-    createJob(input: JobInput!): AdminJob!
-    updateJob(id: ID!, input: JobInput!): AdminJob!
-    updateJobStatus(id: ID!, status: JobStatus!): AdminJob!
+    createJob(input: JobInput!): Job!
+    updateJob(id: ID!, input: JobInput!): Job!
+    updateJobStatus(id: ID!, status: JobStatus!): Job!
   }
 `;
